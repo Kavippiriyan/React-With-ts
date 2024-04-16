87626ea7977b0c2d40b03884685226b75310e20e
Kavippiriyan <kavippiriyan2001@gmail.com>
2024-04-16 19:12:06 +0530

    React with ts Todolist 16/04/2024

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Todo } from './model'
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
 import { MdDone } from 'react-icons/md'
@@ -14,18 +14,30 @@ type Props =
 
 
 const SingleTodo = ({ todo, todos, settodos }: Props) => {
+
+    const [edit, setedit] = useState<boolean>(false);
+    const [edittodo, setedittodo] = useState<string>(todo.todo)
+
     const handledone = (id: number) => {
         settodos(
             todos.map((value) =>
                 value.id === id ? { ...value, isDone: !todo.isDone } : todo)
         )
     }
+
+    const handledelete = (id: number) => {
+        settodos(
+            todos.filter(value => value.id != id)
+        )
+    }
+
+
     return (
         <form className='"todos_single'>
             {todo.isDone ? (<s className='todos_single--text'>{todo.todo}</s>) : <span className='todos_single--text'>{todo.todo}</span>}
 
             <span className="icon"><AiFillEdit /></span>
-            <span className="icon"><AiFillDelete /></span>
+            <span className="icon" onClick={() => handledelete(todo.id)}><AiFillDelete /></span>
             <span className="icon" onClick={() => handledone(todo.id)}><MdDone /></span>
         </form>
     )
